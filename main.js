1dff2b2474dba679ec4b7f5347613721adb1de2a
agavakole <agavakole@gmail.com>
2024-02-16 07:25:17 +0100

    new

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*menu-bar open*/
 $(document).ready(function () {
   $('.menu-toggler').on('click', function () {
@@ -14,55 +13,27 @@ $(document).ready(function () {
 
   /*smooth scroll from the nav*/
   $('nav a[href*="#"]').on('click', function () {
-    $('html, body').animate( {
-        scrollTop: $($(this).attr('href')).offset().top - 100
-    },  2000);
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top - 100,
+      },
+      2000
+    );
   });
 
   /*scroll up*/
   $('#up').on('click', function () {
-    $('html, body').animate( {
-      scrollTop: 0
-    },  2000);
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      2000
+    );
   });
 
   AOS.init({
     easing: 'ease',
     duration: 1800,
-    once: true
+    once: true,
   });
-=======
-/*menu-bar open*/
-$(document).ready(function () {
-  $('.menu-toggler').on('click', function () {
-    $(this).toggleClass('open');
-    $('.top-nav').toggleClass('open');
-  });
-
-  /*close nav*/
-  $('.top-nav .navlink').on('click', function () {
-    $('.menu-toggler').removeClass('open');
-    $('.top-nav').removeClass('open');
-  });
-
-  /*smooth scroll from the nav*/
-  $('nav a[href*="#"]').on('click', function () {
-    $('html, body').animate( {
-        scrollTop: $($(this).attr('href')).offset().top - 100
-    },  2000);
-  });
-
-  /*scroll up*/
-  $('#up').on('click', function () {
-    $('html, body').animate( {
-      scrollTop: 0
-    },  2000);
-  });
-
-  AOS.init({
-    easing: 'ease',
-    duration: 1800,
-    once: true
-  });
->>>>>>> origin/main
 });
